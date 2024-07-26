11cdefbb0aa0815ec476625b3fd0870a55481d32
nichlls <107227159+nichlls@users.noreply.github.com>
2024-07-26 20:25:10 +0100

    2.9

--- part2/b/phonebook-2.6-2.10/src/App.jsx
+++ part2/b/phonebook-2.6-2.10/src/App.jsx
@@ -4,9 +4,12 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
     { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newQuery, setNewQuery] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -18,6 +21,7 @@ const App = () => {
 
     setPersons(persons.concat(nameObject));
     setNewName("");
+    setNewNumber("");
   };
 
   const checkIfExists = (value) => {
@@ -39,9 +43,24 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleQuery = (event) => {
+    setNewQuery(event.target.value);
+  };
+
+  const showPersons =
+    newQuery !== ""
+      ? persons.filter((person) => person.name.includes(newQuery))
+      : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with a <input value={newQuery} onChange={handleQuery} />
+      </div>
+      <div>
+        <h2>Add a new</h2>
+      </div>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -54,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {showPersons.map((person) => (
         <div key={person.name}>
           <p>
             {person.name} {person.number}
